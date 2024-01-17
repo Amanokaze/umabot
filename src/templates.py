66fb015ba502f6e852f8e6518972ea5a111fcd95
amanokaze <oniamano@gmail.com>
2024-01-17 18:05:53 +0900

    quickreplies 추가

--- src/templates.py
+++ src/templates.py
@@ -14,6 +14,18 @@ def response_template(simpletext, response):
                         "items": response
                     }
                 }
+            ],
+            "quickReplies": [
+                {
+                    "label": "다른 스킬 검색",
+                    "action": "message",
+                    "messageText": "스킬"
+                },
+                {
+                    "label": "캐릭터",
+                    "action": "message",
+                    "messageText": "캐릭터"
+                }
             ]
         }
     }
